21149fdb396d00c6651b23352fb3306e83707ea9
chihabMe <chihab.mg.me@gmail.com>
2025-07-22 14:17:13 +0100

    feat: add complete sanity integration with newsletter support
    - Add schemas for posts, authors, categories, newsletter
    - Add TypeScript types for all entities
    - Add GROQ queries for data fetching
    - Add utility functions for CRUD operations
    - Add newsletter subscription functionality

---
+++ src/sanity/index.ts
@@ -0,0 +1,35 @@
+// Sanity client and utilities
+export { client } from "./client";
+
+// Types
+export type { Post, PostPreview, Author, Category, Newsletter } from "./types";
+
+// Data fetching functions
+export {
+  getPosts,
+  getPostBySlug,
+  getPostSlugs,
+  getAuthors,
+  getCategories,
+  getPostsByCategory,
+  getPostsByAuthor,
+  searchPosts,
+  getPostsPaginated,
+  getImageUrl,
+  subscribeToNewsletter,
+  getNewsletterSubscribers,
+  checkEmailSubscription,
+} from "./utils";
+
+// GROQ queries (if you need them directly)
+export {
+  postsQuery,
+  postBySlugQuery,
+  postSlugsQuery,
+  authorsQuery,
+  categoriesQuery,
+  postsByCategoryQuery,
+  postsByAuthorQuery,
+  newsletterSubscribersQuery,
+  newsletterByEmailQuery,
+} from "./queries";
